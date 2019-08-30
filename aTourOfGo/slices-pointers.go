67614417f206471e9f1bacd620176eba49a76b2a
李林 <3086961507@qq.com>
2019-08-30 14:37:02 +0800

    add reverse test

--- aTourOfGo/slices-pointers.go
+++ aTourOfGo/slices-pointers.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	names := [4]string{"John", "Paul", "George", "Ringo"}
 	fmt.Println(names) //"John","Paul","George","Ringo"
-	a := names[0:2]
+	a := names[:2]
 	b := names[1:3]
 	fmt.Println(a, b) //"John","Paul"  "Paul","George"
 	b[0] = "XXX"
